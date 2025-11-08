306d23ff512610ba2788a88490795ea06285302a
ZgMil <zgurskaya.milena@gmail.com>
2025-11-08 17:13:50 -0500

    first commit

---
+++ src/commands/commands.ts
@@ -0,0 +1,17 @@
+
+import { setNotificationInOutlook } from "./outlook";
+
+/* global Office */
+
+// Register the add-in commands with the Office host application.
+Office.onReady(async (info) => {
+  switch (info.host) {
+    
+    case Office.HostType.Outlook:
+      Office.actions.associate("action", setNotificationInOutlook);
+      break;
+    default: {
+      throw new Error(`${info.host} not supported.`);
+    }
+  }
+});
